5f4fa8f09ea3e76d049f38b2b25302cb9e013214
Christian Giannarelli <cbgianna@icloud.com>
2022-01-17 00:01:19 -0300

    Bug-fixing, sw removal

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-if('serviceWorker' in navigator) {
-	// Use the window load event to keep the page load performant
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/sw.js')
-	})
-}
-
 $(function() {
 	let view= $("#view")
 	let counter= 0
